27fb089b4f2d9a20a4efdff74c8b5115350d955c
quintenbosch <r0785961@student.thomasmore.be>
2025-08-11 15:18:04 +0200

    Initial commit

---
+++ tab-service/src/test/java/fact/it/tab/TabApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.tab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
